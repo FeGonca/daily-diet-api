340fcdc2ac38d03cc07d9f3bc20e95a7f3caaf4c
Felipe <felip.goncalves@hotmail.com>
2025-12-12 08:21:57 -0300

    Tabela meal

--- models/user.py
+++ models/user.py
@@ -5,3 +5,14 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=False)
+
+class Meal(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    description = db.Column(db.String(255), nullable=True)
+    diet_flag = db.Column(db.Boolean, nullable=False, default=False)
+    date_created = db.Column(db.DateTime, nullable=False)
+    updated_at = db.Column(db.DateTime, nullable=False)
+
+    # relationships
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
